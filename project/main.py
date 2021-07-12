8955620a0ee50222c4698fcb8a38079ceba61e6e
sanggusti <gustiowinata0@gmail.com>
2021-07-12 14:47:43 +0700

    add celery worker logs and result invokes more details

--- project/main.py
+++ project/main.py
@@ -3,6 +3,7 @@ from fastapi import Body, FastAPI, Form, Request
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from celery.result import AsyncResult
 
 
 app = FastAPI()
@@ -25,4 +26,10 @@ def run_task(payload = Body(...)):
 
 @app.get("/tasks/{task_id}")
 def get_status(task_id):
-    return JSONResponse(task_id)
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id":task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
